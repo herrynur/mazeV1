6cd80b09d93808e873572afabdee84f92fe98039
LAPTOP-4UJII3SP\herin <herinuraliim@gmail.com>
2022-11-18 03:20:59 +0700

    update example

---
+++ example/mode_select_example/mode_select_example.ino
@@ -0,0 +1,37 @@
+// Button 1 mode 1
+// Button 2 mode 2
+// Button 3 mode 3
+// Button 4 mode 4
+// Button 1 dan Button 2 mode 5
+// Button 3 dan Button 4 mode 6
+// Button 1 dan Button 4 kalibrasi
+
+#include "mazeV1.h"
+
+void mazeInit() {
+  
+}
+
+void mode1() {
+  ledblink(2, 400);
+}
+
+void mode2() {
+  ledblink(2, 400);
+}
+
+void mode3() {
+  ledblink(2, 400);
+}
+
+void mode4() {
+  ledblink(2, 400);
+}
+
+void mode5() {
+  ledblink(4, 200);
+}
+
+void mode6() {
+  ledblink(4, 200);
+}
