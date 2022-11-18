b23d2deffa79db04743cbe5fe4921f7445b9822b
LAPTOP-4UJII3SP\herin <herinuraliim@gmail.com>
2022-11-19 02:34:57 +0700

    example baru

---
+++ example/mode_select_example_new/mode_select_example_new.ino
@@ -0,0 +1,64 @@
+// Button 1 mode 1
+// Button 2 mode 2
+// Button 3 mode 3
+// Button 4 mode 4
+// Button 1 dan Button 2 mode 5
+// Button 3 dan Button 4 mode 6
+// Button 1 dan Button 4 kalibrasi
+
+#include "mazeV1.h"
+
+void mazeInit() {
+
+}
+
+// Mode kanan/kiri utama 1
+void mode1() {
+  ledblink(1, 400);
+}
+
+void mode2() {
+  ledblink(2, 400);
+}
+
+void mode3() {
+  ledblink(3, 400);
+}
+
+void mode4() {
+  ledblink(4, 400);
+}
+
+void mode5() {
+  ledblink(5, 400);
+}
+
+void mode6() {
+  ledblink(6, 400);
+}
+
+// Mode kanan/kiri utama 2
+
+void mode7() {
+  ledblink(7, 400);
+}
+
+void mode8() {
+  ledblink(8, 400);
+}
+
+void mode9() {
+  ledblink(9, 400);
+}
+
+void mode10() {
+  ledblink(10, 400);
+}
+
+void mode11() {
+  ledblink(11, 400);
+}
+
+void mode12() {
+  ledblink(12, 400);
+}
